579753490f79919c8348435a4b9d76ca2a529dd5
Arthur Vassoler <a.h.vassoler@gmail.com>
2024-06-02 21:58:29 -0300

    feat: create background and add global styles

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+import GlobalStyles from "./components/globalStyles"
+
+const BackgroudGradient = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width:100%;
+  height: 100vh;
+`
+
+function App() {
+
+  return (
+    <BackgroudGradient>
+      <GlobalStyles />
+    </BackgroudGradient>
+  )
+}
+
+export default App
